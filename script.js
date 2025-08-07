2ae0b9fc5bf267e0638eca226c1eaa2544972ac9
shambhawi12 <shambhawi36@gmail.com>
2025-08-08 01:05:30 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-
 const h = document.querySelector("#h");
 const b = document.body;
 
-let base = (e) => {
-    var x = e.pageX / window.innerWidth - 0.5;
-    var y = e.pageY / window.innerHeight - 0.5;
-    h.style.transform = `
-        perspective(90vw)
-        rotateX(${ y * 10  + 75}deg)
-        rotateZ(${ -x * 25  + 45}deg)
-        translateZ(-9vw)
-    `;
+function base(e) {
+  let x, y;
+
+  if (e.touches) {
+    // Touch event
+    x = e.touches[0].clientX / window.innerWidth - 0.5;
+    y = e.touches[0].clientY / window.innerHeight - 0.5;
+  } else {
+    // Pointer or mouse
+    x = e.pageX / window.innerWidth - 0.5;
+    y = e.pageY / window.innerHeight - 0.5;
+  }
+
+  h.style.transform = `
+    perspective(90vw)
+    rotateX(${y * 10 + 75}deg)
+    rotateZ(${-x * 25 + 45}deg)
+    translateZ(-9vw)
+  `;
 }
+
+// Desktop & pointer support
+b.addEventListener("pointermove", base);
+// Mobile & touch support
+b.addEventListener("touchmove", base);
+
 
 b.addEventListener("pointermove", base);
 
